156b6fb4ab419c7798b84025932c4a9f7f02973e
Yohan Mayorga <yohanucv@gmail.com>
2024-01-30 10:17:15 -0400

    footer file deleted

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <div className="w-full bg-black text-white fixed bottom-0 flex items-center justify-center z-100 ">
-      Created by Yohan Mayorga
-    </div>
-  );
-}
